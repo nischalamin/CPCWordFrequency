828c2d184a78eced81e27d64e2dd2e0eeb233818
Timothy Koeth <timothykoeth@Timothys-MacBook-Pro.local>
2015-09-10 20:41:21 -0400

    Now output patent numbers for a CPC symbol to a text file

--- scrapemergeCPC.rb
+++ scrapemergeCPC.rb
@@ -21,50 +21,54 @@ query=""
 numberhits = 0
 body = ""
 pagestotal = 0
+numofrows = 0
+numofrowsindex = 0
+patnoarray = Array.new
 # Use stdargs as way to specify which CPC class will be inputted - first put in class (i.e. H04B7) then second arg to be the group (i.e. 0413) do not add slashes
+# H04B10 70 is a good test class to use - it only returns 143 patents
 cpcclass = ARGV[0]
 cpcgroup = ARGV[1]
 cpcpage = Mechanize.new
 #This query returns a certain number of hits paginated into 50 at a time. So, we will need to call this X hits / 50 times to get all the patents issued
-query = "http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&u=%2Fnetahtml%2FPTO%2Fsearch-adv.htm&r=0&p=1&f=S&l=50&Query=CPC%2F" + cpcclass + "%2F" + cpcgroup + "&d=PTXT"
+queryparam = "&Query=CPC%2F" + cpcclass + "%2F" + cpcgroup
+RSparam = "&RS=CPC%2F" + cpcclass + "%2F" + cpcgroup
+searchjumpparam = "&Srch1=" + cpcclass + "%2F" + cpcgroup + ".CPC.&StartAt=Jump+To&StartNum="
+query = "http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&u=%2Fnetahtml%2FPTO%2Fsearch-adv.htm&r=0&p=1&f=S&l=50" + queryparam + "&d=PTXT"
 
 page = cpcpage.get(query)
 # now scrape out the total number returned - this will be the text value of the third STRONG tag found:
 numberhits = page.parser.xpath("//strong")[2].text.strip.to_i
 pagestotal = (numberhits / 50).ceil
-puts "Number of results: " + numberhits.to_s
-
+# puts "Number of results: " + numberhits.to_s
+jumptoquery = "http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&u=%2Fnetahtml%2FPTO%2Fsearch-adv.htm&r=0&f=S&l=50&d=PTXT" + RSparam + queryparam + "&TD=" + numberhits.to_s + searchjumpparam
 # Now get the paginated tables of results. The results begin on the second TABLE tag down
 
 pagetags = page.parser.xpath("//table")[1]
 # we want to iterate through the table listing of patents for this page
-patentlisting = pagetags.xpath("//tr")
-patentpage = patentlisting[2]
-#pagetags.each_with_index do |row, i|
-  # Get the patent text link to follow - they start at TD/A/HREF number 6 - then you alter the query variable r (r for return) up to 50, for the 50ith return for that page, then go to the next page by changing the variable
-  # p = 2 and so on. The page variable must increment after every 50, but the r variable keeps counting up until you hit the total number of returns
-  patentnoURL =  patentpage.xpath("//td/a/@href")[6].text
-  # Get just the static portions of this URL
-  prefixURL = "http://patft.uspto.gov"
-  uptoRURL = patentnoURL.split("&r=")[0] # 0 gives up everything up to the &r= part
-  afterpURL = patentnoURL.split("&p=")[1] # 1 gives us everything after &p part. We know whats between since r and p variables are right next to each other
-# Now we go through all of the hits, extract the text, and concatentate into a big file.
-returnno = 1
-pageno = 1
-until returnno > numberhits
-  # follow the link via a new Mechanize page
-  patenttextURL = prefixURL + uptoRURL + "&r=" + returnno.to_s + "&p=" + pageno.to_s + afterpURL[1..-1]
-### TO DO STILL IN THIS CODE
-# Call the page in the patenttextURL
-  #patenttext = cpcpage.get(patenttextURL)
-# Scrape text of patent and output to an aggregate file - maybe best to do this with Ruby string library since theres no real structure on the returned HTML
-  #body = patenttext.parser.xpath("//body").text
-  #body = body.split("DETAILED DESCRIPTION")[1]
-# Spit our the detailed description text to a text file that's aggregating all the patent text so far
+patentlisting = pagetags.xpath(".//tr")
+j = 1
 
-# Increment page and result variables accordingly
-  if(returnno % 50 == 0) # will be zero for multiples of 50 - this signals when we need to increment the page var
-    pageno = pageno + 1
+until numofrowsindex > (numberhits - 1)
+  j = 1
+  if numofrowsindex != 0
+   page = cpcpage.get(jumptoquery+(numofrowsindex+1).to_s)
+   pagetags = page.parser.xpath("//table")[1]
+   patentlisting = pagetags.xpath(".//tr")
   end
-  returnno = returnno + 1
+  until (j > 50) || (numofrowsindex > (numberhits - 1))
+    # Scrape one page at a time, get just the patent number, then the gsub is a regex to remove the commas
+    patnoarray[numofrowsindex] = patentlisting[j].xpath(".//td/a")[0].text.gsub(/[\s,]/ ,"")
+    #puts "patent no: " + patnoarray[numofrowsindex]
+    j = j + 1
+    numofrowsindex = numofrowsindex + 1
+  end
+end
+
+# The patnoarray will contain a full list of all the patents under that CPC classification now.
+#puts "Number of Pat No array members: " + patnoarray.count.to_s + " Which should equal the count scraped from the page: " + numberhits.to_s
+# Export list (CR separated) into a text file:
+File.open("patentnums.txt", "w") do |file|
+  patnoarray.each { |patno| 
+   file.puts patno
+  }
 end
